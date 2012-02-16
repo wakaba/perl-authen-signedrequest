c563a604ddf80976272eb9463b2b8a11d0063f83
wakabatan <wakabatan@localhost.localdomain>
2012-02-16 18:50:00 +0900

    Decoding signed_request

---
+++ bin/dump-signed-request.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Path::Class;
+use lib file(__FILE__)->dir->parent->subdir('lib')->stringify;
+use lib file(__FILE__)->dir->parent->subdir('modules', 'jsonfunctionsxs', 'lib')->stringify;
+use Authen::SignedRequest;
+use Data::Dumper;
+
+my ($signed, $key) = @ARGV;
+$signed =~ s/^signed_request=//;
+
+$Authen::SignedRequest::Warn = 1;
+my $data = Authen::SignedRequest->verify_signed_request($signed, $key);
+
+print "Valid: " . ($data ? 'Yes' : 'No') . "\n";
+if ($data) {
+    print "Data:\n";
+    print Dumper $data;
+}
